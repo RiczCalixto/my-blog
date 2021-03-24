341c2643ec9c47990e8216456821267c98514127
RiczCalixto <rcalixtos@gmail.com>
2021-03-23 21:14:55 -0300

    feat: cleanup files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My personal blog`,
+    description: `A blog about my journey`,
+    author: `@ricsblog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/components/layout.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+
+const Layout: React.FC = ({ children }) => <main>{children}</main>
+
+export default Layout
