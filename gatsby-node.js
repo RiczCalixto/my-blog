a9832f753c8412f082cc64566db9125342b82d52
RiczCalixto <rcalixtos@gmail.com>
2021-04-24 15:23:31 -0300

    fix: template path at gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -81,7 +81,7 @@ exports.createPages = ({ graphql, actions }) => {
     posts.forEach(({ node, next, previous }) => {
       createPage({
         path: node.fields.slug,
-        component: path.resolve(`./src/templates/blog-post.tsx`),
+        component: path.resolve(`./src/templates/BlogPost.tsx`),
         context: {
           // Data passed to context is available
           // in page queries as GraphQL variables.
@@ -98,7 +98,7 @@ exports.createPages = ({ graphql, actions }) => {
     Array.from({ length: numPages }).forEach((_, index) => {
       createPage({
         path: index === 0 ? `/` : `/page/${index + 1}`,
-        component: path.resolve(`./src/templates/blog-list.tsx`),
+        component: path.resolve(`./src/templates/BlogList.tsx`),
         context: {
           limit: postsPerPage,
           skip: index * postsPerPage,
