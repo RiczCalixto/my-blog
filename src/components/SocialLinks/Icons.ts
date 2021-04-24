8890016e70ed6b998d049c0d201624ac1853130b
RiczCalixto <rcalixtos@gmail.com>
2021-04-24 15:58:12 -0300

    fix: remove files from tree and index

--- src/components/SocialLinks/Icons.ts
+++
@@ -1,9 +0,0 @@
-import { Github } from "@styled-icons/feather/Github"
-import { LogoMedium as Medium } from "@styled-icons/ionicons-solid/LogoMedium"
-import { Linkedin } from "@styled-icons/feather/Linkedin"
-
-export const Icons = {
-  Github,
-  Medium,
-  Linkedin,
-}
